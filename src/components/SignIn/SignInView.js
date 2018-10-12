fae50e418b1d20b4f67e34bc97cb1247f7b6ede4
martinapps <bruce.martinduo@gmail.com>
2018-10-12 17:33:05 -0400

    Improve ErrorBounday messages

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -7,7 +7,7 @@ import './styles.css'
 const SignInView = ({ onSubmit, onSignUp, handleChange, handleDemoMode, reference }) => {
   return (
     <div>
-      0.1.15
+      0.1.16
       <span className={'header'}>
         <h1>Log In</h1>
       </span>
