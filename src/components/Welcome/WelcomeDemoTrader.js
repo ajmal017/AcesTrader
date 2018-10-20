69058f7af68fd745f6a7049e87985a3e811ab5c5
martinapps <bruce.martinduo@gmail.com>
2018-10-20 17:09:20 -0400

    Change "demo mode" to "guest mode" in Welcome page

--- src/components/Welcome/WelcomeDemoTrader.js
+++ src/components/Welcome/WelcomeDemoTrader.js
@@ -18,14 +18,14 @@ const welcomeDemoTrader = () => {
         ideas.
       </p>
       <p>
-        This demo mode has no interaction with any brokerage firm. Price data for charts is obtained from the IEX stock exchange. Order executions are simulated using the IEX price
-        data for fill prices. These prices are obtained at the time of you order, and they are the last price from the ticker feed. So after trading hours they are stale, but still
-        good enough for the demo's simulated swing trading.
+        This guest mode has no interaction with any brokerage firm. Price data for charts is obtained from the IEX stock exchange. Order executions are simulated using the IEX
+        price data for fill prices. These prices are obtained at the time of you order, and they are the last price from the ticker feed. So after trading hours they are stale, but
+        still good enough for the guest mode's simulated swing trading.
       </p>
       <p>
-        There's another limitation to the demo mode. There is no access to cloud storage. The data for your lists are saved in the local storage of your browser. If you create your
-        lists on your desktop computer, you won't be able to see the same data using the program on your iPad or iPhone. So make your lists in the browser and device you will use
-        for your paper money trading sessions. You can of course, use another browser and device to persue a second trading strategy, without affecting the other.
+        There's another limitation to the guest mode. There is no access to cloud storage. The data for your lists are saved in the local storage of your browser. If you create
+        your lists on your desktop computer, you won't be able to see the same data using the program on your iPad or iPhone. So make your lists in the browser and device you will
+        use for your paper money trading sessions. You can of course, use another browser and device to persue a second trading strategy, without affecting the other.
       </p>
       <p>Click the AcesTrader menu title for more information.</p>
     </div>
