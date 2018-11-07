959ac15914b35d81483050939e336adeb70764d7
martinapps <bruce.martinduo@gmail.com>
2018-11-07 08:14:05 -0500

    Bump to v0.1.34. Add firebase-debug.log to .gitignore

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -13,7 +13,7 @@ const SignInView = ({ onSubmit, onSignUp, handleChange, handleDemoMode, handleDe
 
   return (
     <div>
-      0.1.33
+      0.1.34
       <span className={'header'}>
         <h1>Sign In</h1>
       </span>
