f1df4839f1279f12dc05965ce83a326568514a7f
martinapps <bruce.martinduo@gmail.com>
2018-11-10 09:15:37 -0500

    Change undefined 'weekly' to 'false' for props to Charting components. Present mockSymbols only to localTrader in developmen mode.

--- src/components/ManageBuyProspects/index.js
+++ src/components/ManageBuyProspects/index.js
@@ -18,17 +18,10 @@ class ManageBuyProspects extends Component {
 
   // "'TQQQ','UDOW','UPRO','QLD','SPXL','MCHI','SSO','MTUM','XLK','QQQ','FXI','IWF','XLF','SPYG','DIA','EWJ','EWI','EWS','IVV','VOO','SPY','ACWI','VT','EWH','IEFA','SCHF','VEU','EFA','CWB','EZU','USMV','EFV'",
   // 'UWT, UCO, TNA, FAS, GUSH, ERX, USO, MCHI, XME, XOP, KRE, IJR, FXI, KBE, XLF, DIA, IWM, PDBC, EWJ, XLE, ACWI, EWH, EWQ, VTV, EWY, SCHF, IEFA, VEU, VEA, EFA, EZU, VGK, EWU, HEZU, OIH, TBT ',
-  // Note: For testing and demonstration a list of ETF ssymbols
-  //is passed to ManageProspects to use as default input to the prospects list
+  // "TRTY hyd amzn msft alfa ACWI DBC DIA EFA ERX EWH EWJ FAS GUSH IEFA IJH KBE KRE MCHI MDY OIH TBT VEA VFH VT VTV XLE XLF XLI XOP XRT"
+  // Note: For testing and debugging a list of ETF ssymbols is passed to ManageProspects to use as default input to the prospects list
   render() {
-    return (
-      <ManageProspects
-        tradeSide={'Swing Buys'}
-        handleClick={this.handleClick}
-        mockSymbols="shv hyd amzn msft alfa DBC DBO ERX GUSH IBB UCO USO UWT XBI XES XLE XOP"
-        // mockSymbols="TRTY hyd amzn msft alfa ACWI DBC DIA EFA ERX EWH EWJ FAS GUSH IEFA IJH KBE KRE MCHI MDY OIH TBT VEA VFH VT VTV XLE XLF XLI XOP XRT"
-      />
-    )
+    return <ManageProspects tradeSide={'Swing Buys'} handleClick={this.handleClick} mockSymbols="shv hyd amzn msft alfa vb vt vti vtv trty" />
   }
 }
 
