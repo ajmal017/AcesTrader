50fa9c4a27d4dcc309734ef782e3bb93ff750a81
martinapps <bruce.martinduo@gmail.com>
2018-11-20 17:27:02 -0500

    Rename axiosHelpers to apiAxiosHelpers

--- src/lib/appSymbolDataObject.js
+++ src/lib/appSymbolDataObject.js
@@ -1,5 +1,10 @@
 // appSymbolDataObject.js
 
+// This data is an array of objects, each with info about one symbol,
+// created by ManageProspects calling putSymbolDataObjects when verifying a list of new prospect symbols.
+// The array is used later with calls to getSymbolDataObject from addWatchPriceAndIssueTypeAsync()
+// in the thunkEditListObjects.js file.
+
 let currentSymbolDataObjects = []
 // let dummySymbolDataObjects = [{ symbol: 'ALFA' }, { symbol: 'IBM' }]
 
