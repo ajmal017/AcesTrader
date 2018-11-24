db7a4322658f6a006538fe8b1a3879fcdbda8ad7
martinapps <bruce.martinduo@gmail.com>
2018-11-24 17:57:27 -0500

    bumped merged version to 0.1.54

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -13,7 +13,7 @@ const SignInView = ({ onSubmit, onSignUp, handleChange, handleDemoMode, handleDe
 
   return (
     <div>
-      0.1.53
+      0.1.54
       <span className={'header'}>
         <h1>Sign In</h1>
       </span>
