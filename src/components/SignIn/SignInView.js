5f6b51bdb069431b099e01c612a1d6ce3ee36097
martinapps <bruce.martinduo@gmail.com>
2018-11-24 21:31:40 -0500

    Guild v0.1.54, bump to 55

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -13,7 +13,7 @@ const SignInView = ({ onSubmit, onSignUp, handleChange, handleDemoMode, handleDe
 
   return (
     <div>
-      0.1.54
+      0.1.55
       <span className={'header'}>
         <h1>Sign In</h1>
       </span>
