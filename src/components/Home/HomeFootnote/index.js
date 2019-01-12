2bb90699ca1ba9b194cf05e060fd71baa5b8a87c
martinapps <bruce.martinduo@gmail.com>
2019-01-12 16:33:11 -0500

    Change copyright to 2018-2019

--- src/components/Home/HomeFootnote/index.js
+++ src/components/Home/HomeFootnote/index.js
@@ -3,20 +3,20 @@ import './styles.css'
 class HomeFootnote extends Component {
   render() {
     return (
-      <div className="footnote">
+      <div className='footnote'>
         <span>
-          Copyright &copy; 2018{' '}
-          <a href={process.env.PUBLIC_URL + '/bm.html'} target="_blank" rel=" noopener noreferrer">
+          Copyright &copy; 2018-2019{' '}
+          <a href={process.env.PUBLIC_URL + '/bm.html'} target='_blank' rel=' noopener noreferrer'>
             Bruce Martin
           </a>
         </span>
-        <span className="duckimagecredit">
+        <span className='duckimagecredit'>
           Duck image by{' '}
-          <a href="http://delapouite.com" target="_blank" rel=" noopener noreferrer">
+          <a href='http://delapouite.com' target='_blank' rel=' noopener noreferrer'>
             Delapouite
           </a>{' '}
           under{' '}
-          <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener noreferrer">
+          <a href='http://creativecommons.org/licenses/by/3.0/' target='_blank' rel='noopener noreferrer'>
             CC BY 3.0
           </a>
         </span>
