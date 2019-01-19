dbf5f42c28cabd293624fdafe9fd3fa3335c2ab0
martinapps <bruce.martinduo@gmail.com>
2019-01-19 09:56:46 -0500

    Move styled-component consts outside of render(). Add backgroundDollar to SignIn and WelcomeRealTrader views.

--- src/components/Home/HomeJumbotron/index.js
+++ src/components/Home/HomeJumbotron/index.js
@@ -3,33 +3,30 @@ import logo from './../../../images/duck.svg'
 import './styles.css'
 import styled from 'styled-components'
 
+const HeaderContainer = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+`
+
 class HeadJumbotron extends Component {
   render() {
-    const HeaderContainer = styled.section`
-      display: flex;
-      flex-flow: row wrap;
-      justify-content: center;
-      align-items: center;
-    `
     return (
-      <div>
-        <div id='featured' className='jumbotron home-row0 home-row0-background'>
-          {/* <div> */}
-          {/* <HeaderContainer className='container text-center'> */}
-          <HeaderContainer>
-            <h1>
-              <span className='duck-jumbotron'>AcesTrader</span>
-            </h1>
-          </HeaderContainer>
-          <HeaderContainer>
-            <h3>
-              <img id='duck-home' src={logo} alt='Logo' width='40' />
-              <em>
-                <span className='duck-slogan'>An End-of-Day Trading Tool</span>
-              </em>
-            </h3>
-          </HeaderContainer>
-        </div>
+      <div id='featured' className='jumbotron home-row0 home-row0-background'>
+        <HeaderContainer>
+          <h1>
+            <span className='duck-jumbotron'>AcesTrader</span>
+          </h1>
+        </HeaderContainer>
+        <HeaderContainer>
+          <h3>
+            <img id='duck-home' src={logo} alt='Logo' width='40' />
+            <em>
+              <span className='duck-slogan'>An End-of-Day Trading Tool</span>
+            </em>
+          </h3>
+        </HeaderContainer>
       </div>
     )
   }
