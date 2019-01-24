a2cf712ed80a8ff599e67457835f9b03f37063a9
martinapps <bruce.martinduo@gmail.com>
2019-01-24 16:48:37 -0500

    Bump to v0.2.1

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -12,7 +12,7 @@ const SignInView = ({ onSubmit, onSignUp, handleDemoMode, handleDemoInfo, email,
   return (
     <div>
       <div className={'backgroundDollar'}>
-        0.1.99
+        0.2.1
         <span className={'header'}>
           <span>Sign In</span>
         </span>
