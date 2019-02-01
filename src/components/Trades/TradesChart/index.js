f7a08d3d08da04b29649093320d530f3bc8dc623
martinapps <bruce.martinduo@gmail.com>
2019-02-01 17:41:54 -0500

    Customize TradesChart for vertical tick labels and y-axis label

--- src/components/Trades/TradesChart/index.js
+++ src/components/Trades/TradesChart/index.js
@@ -6,37 +6,37 @@ import withSizes from 'react-sizes'
 import { BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 // const data = [
-//   { symbol: 'AMZN', GainPercentage: 8.4 },
-//   { symbol: 'MSFT', GainPercentage: 13.98 },
-//   { symbol: 'ALFA', GainPercentage: -9.8 },
-//   { symbol: 'VTI', GainPercentage: 3.908 },
-//   { symbol: 'VTC', GainPercentage: 4.8 },
-//   { symbol: 'VNQI', GainPercentage: -3.8 },
-//   { symbol: 'VWO', GainPercentage: 4.3 },
+//   { symbol: 'AMZN', Gain: 8.4 },
+//   { symbol: 'MSFT', Gain: 13.98 },
+//   { symbol: 'ALFA', Gain: -9.8 },
+//   { symbol: 'VTI', Gain: 3.908 },
+//   { symbol: 'VTC', Gain: 4.8 },
+//   { symbol: 'VNQI', Gain: -3.8 },
+//   { symbol: 'VWO', Gain: 4.3 },
 
-//   { symbol: 'AMZN', GainPercentage: 4.0 },
-//   { symbol: 'MSFT', GainPercentage: -3.0 },
-//   { symbol: 'ALFA', GainPercentage: -2.0 },
-//   { symbol: 'VTI', GainPercentage: 2.78 },
-//   { symbol: 'VTC', GainPercentage: -18.9 },
-//   { symbol: 'VNQI', GainPercentage: 2.39 },
-//   { symbol: 'VWO', GainPercentage: 3.49 },
+//   { symbol: 'AMZN', Gain: 4.0 },
+//   { symbol: 'MSFT', Gain: -3.0 },
+//   { symbol: 'ALFA', Gain: -2.0 },
+//   { symbol: 'VTI', Gain: 2.78 },
+//   { symbol: 'VTC', Gain: -18.9 },
+//   { symbol: 'VNQI', Gain: 2.39 },
+//   { symbol: 'VWO', Gain: 3.49 },
 
-//   { symbol: 'AMZN', GainPercentage: 8.4 },
-//   { symbol: 'MSFT', GainPercentage: 13.98 },
-//   { symbol: 'ALFA', GainPercentage: -9.8 },
-//   { symbol: 'VTI', GainPercentage: 3.908 },
-//   { symbol: 'VTC', GainPercentage: 4.8 },
-//   { symbol: 'VNQI', GainPercentage: -3.8 },
-//   { symbol: 'VWO', GainPercentage: 4.3 },
+//   { symbol: 'AMZN', Gain: 8.4 },
+//   { symbol: 'MSFT', Gain: 13.98 },
+//   { symbol: 'ALFA', Gain: -9.8 },
+//   { symbol: 'VTI', Gain: 3.908 },
+//   { symbol: 'VTC', Gain: 4.8 },
+//   { symbol: 'VNQI', Gain: -3.8 },
+//   { symbol: 'VWO', Gain: 4.3 },
 
-// { symbol: 'AMZN', GainPercentage: 4.0 },
-// { symbol: 'MSFT', GainPercentage: -3.0 },
-// { symbol: 'ALFA', GainPercentage: -2.0 },
-// { symbol: 'VTI', GainPercentage: 2.78 },
-// { symbol: 'VTC', GainPercentage: -18.9 },
-// { symbol: 'VNQI', GainPercentage: 2.39 },
-// { symbol: 'VWO', GainPercentage: 3.49 },
+// { symbol: 'AMZN', Gain: 4.0 },
+// { symbol: 'MSFT', Gain: -3.0 },
+// { symbol: 'ALFA', Gain: -2.0 },
+// { symbol: 'VTI', Gain: 2.78 },
+// { symbol: 'VTC', Gain: -18.9 },
+// { symbol: 'VNQI', Gain: 2.39 },
+// { symbol: 'VWO', Gain: 3.49 },
 // ]
 
 class TradesChart extends React.Component {
@@ -50,21 +50,22 @@ class TradesChart extends React.Component {
       const exitedPrice = tradeObject.exitedPrice
       const tradePercentGainTemp = ((100 * (exitedPrice - enteredPrice)) / enteredPrice).toFixed(1)
       const tradePercentGain = tradeSide === 'Shorts' ? -tradePercentGainTemp : tradePercentGainTemp
-      return { symbol: tradeObject.symbol, GainPercentage: tradePercentGain }
+      return { symbol: tradeObject.symbol, Gain: tradePercentGain, fillColor: '#00ca9d' }
     })
     const data = data1.reverse()
 
     return (
       <div id='tradeschartcontainer'>
-        <BarChart width={this.props.width - 40} height={200} data={data} margin={{ top: 35, right: 30, left: 20, bottom: 5 }}>
+        <BarChart width={this.props.width - 40} height={200} data={data} margin={{ top: 35, right: 30, left: 20, bottom: 15 }}>
           <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='symbol' />
-          {/* <XAxis /> */}
-          <YAxis />
-          {/* <Tooltip /> */}
+          <XAxis dataKey='symbol' tick={{ value: 'symbol', angle: -90, textAnchor: 'end' }} />
+          <YAxis label={{ value: 'Gain %', angle: -90, position: 'outsideLeft' }} /> {/* <Tooltip /> */}
+          <Tooltip />
           {/* <Legend /> */}
           <ReferenceLine y={0} stroke='#000' />
-          <Bar dataKey='GainPercentage' fill='#82ca9d' />
+          {/* <Bar dataKey='Gain' fill='fillColor' minPointSize={3} /> */}
+          {/* <Bar dataKey='Gain' fill={{ value: '#ff0000' }} minPointSize={3} /> */}
+          <Bar dataKey='Gain' fill='#91a7ff' minPointSize={3} />
         </BarChart>
       </div>
     )
