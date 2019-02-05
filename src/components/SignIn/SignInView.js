a2231604ffd8cd517422965f65d67f615f7b3ce2
martinapps <bruce.martinduo@gmail.com>
2019-02-04 22:18:34 -0500

    Fix Regexpression syntax. Bump version to 0.2.7

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -12,7 +12,7 @@ const SignInView = ({ onSubmit, onSignUp, handleDemoMode, handleDemoInfo, email,
   return (
     <div>
       <div className={'backgroundDollar'}>
-        0.2.6
+        0.2.7
         <span className={'header'}>
           <span>Sign In</span>
         </span>
