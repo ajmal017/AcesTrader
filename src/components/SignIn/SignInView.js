87e6b185eb1a2d6fb9e1797a2a03a76667f2f1c3
martinapps <bruce.martinduo@gmail.com>
2019-02-05 08:51:50 -0500

    Remove space between time and "am". v0.2.8

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -12,7 +12,7 @@ const SignInView = ({ onSubmit, onSignUp, handleDemoMode, handleDemoInfo, email,
   return (
     <div>
       <div className={'backgroundDollar'}>
-        0.2.7
+        0.2.8
         <span className={'header'}>
           <span>Sign In</span>
         </span>
