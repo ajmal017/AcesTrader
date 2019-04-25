c71a0f938ca54112a55401dd53de4f91f02398bd
martinapps <bruce.martinduo@gmail.com>
2019-04-25 15:04:29 -0400

    Add apiGetIEXData.js

---
+++ src/lib/apiGetIEXData.js
@@ -0,0 +1,47 @@
+// apiGetIEXData.js
+
+import axios from 'axios'
+import iexData from '../iex.json'
+
+export const getIEXData = async function (symbol, range, closeOnly, useSandbox = false) {
+    const basehtml = useSandbox ? `${iexData.BasehtmlSandbox}` : `${iexData.BasehtmlCloud}`
+    const version = iexData.Version
+    const query = closeOnly ? 'chartCloseOnly=true' : 'filter=date,open,high,low,close,volume'
+    const token = useSandbox ? `token=${iexData.PublishableTestToken}` : `token=${iexData.PublishableToken}`
+    // console.log(`${basehtml}${version}/stock/${symbol}/chart/${range}?${query}&${token}`)
+    // debugger // pause for developer
+    try {
+        const request = axios
+            .get(`${basehtml}${version}/stock/${symbol}/chart/${range}?${query}&${token}`)
+        let res = await request
+        let values = res.data
+        let data = values.map((obj) => {
+            let date = obj.date
+            date = date + 'T15:00:00.000Z'
+            obj.date = new Date(date)
+            return obj
+        })
+        return data
+    } catch (error) {
+        console.log('getIEXData axios error:', error.message)
+        // alert('getIEXData axios error: ' + error.message) //rude interruption to user
+        debugger // pause for developer
+    }
+}
+
+
+    //     .then((res) => {
+    //         let values = res.data
+    //         let data = values.map((obj) => {
+    //             let date = obj.date
+    //             date = date + 'T15:00:00.000Z'
+    //             obj.date = new Date(date)
+    //             return obj
+    //         })
+    //         return data
+    //     })
+    //     .catch((error) => {
+    //         debugger // pause for developer
+    //         return error
+    //     })
+    // return request //let caller handle the promise's .then/.catch completion}
