28048ddb7ea540f5afda573cfc382b4460d74833
martinapps <bruce.martinduo@gmail.com>
2019-05-13 17:21:59 -0400

    Translate nextState to 1-word term for dashbard display

--- src/components/Charts/Chartcell/index.js
+++ src/components/Charts/Chartcell/index.js
@@ -52,8 +52,8 @@ class Chartcell extends Component {
     this.closeOnly = false // New for IEX Cloud data
 
     // ****************************************************
-    this.useSandbox = process.env.NODE_ENV === 'development' ? true : false // development gets junk ohlc values to test the app, but free downloads. 
-    // this.useSandbox = false // Set to false to test the stateMachine logic with real ohlc values, but usage rates apply
+    // this.useSandbox = process.env.NODE_ENV === 'development' ? true : false // development gets junk ohlc values to test the app, but free downloads. 
+    this.useSandbox = false // Set to false to test the stateMachine logic with real ohlc values, but usage rates apply
     // ****************************************************
 
     this.data = null
