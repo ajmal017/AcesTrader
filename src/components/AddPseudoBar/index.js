3e614530bb5ab55b01e1ebb973393082e6496a9d
martinapps <bruce.martinduo@gmail.com>
2019-10-12 13:52:43 -0400

    Remove obsolete debugging statements from AddPseudoBar

--- src/components/AddPseudoBar/index.js
+++ src/components/AddPseudoBar/index.js
@@ -37,9 +37,8 @@ const AddPseudoBar = () => {
     for (let i = 0; i < numberOfBatches; i++) {
       let symbolsBatch = symbols.slice(i * BATCH_SIZE, (i + 1) * BATCH_SIZE)
       await makeBatchOfPseudoBars(symbolsBatch) // the pseudo bars are cached in allPseudoBars[] indexed by symbol
-      // debugger //bcm
     }
-    // returns having filled allPseudoBars[] with a pseudoBar for each symbol
+    // This function returns after having filled allPseudoBars[] with a pseudoBar for each symbol
   }
 
   const makeBatchOfPseudoBars = async (symbols) => {
@@ -61,11 +60,8 @@ const AddPseudoBar = () => {
     // NOTE: This will not include pre or post market prices.
 
     try {
-      // const request = axios.get(`${basehtml}${version}/stock/market/batch?types=price&symbols=${symbols.join(',')}&${token}`)
       const request = axios.get(`${basehtml}${version}/stock/market/batch?types=quote&symbols=${symbols.join(',')}&filter=${filters.join(',')}&${token}`)
       let res = await request
-      // debugger //bcm
-
       let values = res.data
       for (let symbol in values) {
         let data = values[symbol]
@@ -78,7 +74,7 @@ const AddPseudoBar = () => {
           let fakeDate = date
           let key = symbol
           let pseudoBar = { pseudoBar: true, symbol: symbol, open: fakeOpen, close: fakeClose, high: fakeHigh, low: fakeLow, date: fakeDate, volume: 10000 }
-          allPseudoBars[key] = pseudoBar
+          allPseudoBars[key] = pseudoBar // add this bar to the array
         } else {
           debugger // pause for developer
         }
@@ -95,11 +91,10 @@ const AddPseudoBar = () => {
     const symbol = extractSymbolFromSymbolKey(symbolKey)
     const pseudoBar = allPseudoBars[symbol]
     const symbolData = await loadLocalDatabase(symbolKey) // get price series from the cache
-    // debugger //bcm
 
     let currentLastBar = symbolData[symbolData.length - 1]
 
-    // BCM Hack to workaround leftover bad bar. This bug discovered 5/20/19, Monday after weekend of testing AddPseudoBar
+    // Hack to workaround leftover bad bar. This problem discovered Monday 5/20/19, after weekend of testing AddPseudoBar
     if (currentLastBar === undefined) {
       symbolData.pop() // undefined array element result of testing AddPseudoBar?
       currentLastBar = symbolData[symbolData.length - 1] // get the new last bar
@@ -110,7 +105,6 @@ const AddPseudoBar = () => {
     }
     symbolData.push(pseudoBar) // add the newly created pseudoBar
     await saveLocalDatabase(symbolKey, symbolData) // save the modified symbol data
-    // debugger //bcm
     // let testing = await loadLocalDatabase(symbolKey) // test get object back
     // debugger // pause for developer
   }
@@ -128,8 +122,6 @@ const AddPseudoBar = () => {
   const buildPseudoBars = async () => {
     //the local DB contains last trading day symbol price data, returns the DB daysOld number if data exists
     const daysOld = await setTheLocalDatabase(date)
-    // debugger //bcm
-
     if (theDay === 6 || theDay === 0) {
       // Today is Saturday or Sunday, all price series have correct last trading day bar
       errorMessage = 'Today is a weekend, all price series have correct last day trading bar'
@@ -142,14 +134,12 @@ const AddPseudoBar = () => {
       errorMessage = 'The cache of symbol price data is empty, load the charts first.'
       setDataReady({ loading: false, error: errorMessage })
     } else {
-      // Testing error handling
+      // Create fake error to test error handling
       // errorMessage = 'The cache of symbol price data is empty, FAKE FAKE.'
       // setDataReady({ loading: false, error: errorMessage })
 
       // We need to create an array of symbols of the securities in the local data base cache
       const allKeys = await getLocalDatabaseKeys() // get the keys of objects from the cache
-      // debugger //bcm
-
       // remove the MetaKey
       const symbolKeys = allKeys.filter((key) => {
         return key !== 'MetaKey-DateObject'
@@ -167,32 +157,17 @@ const AddPseudoBar = () => {
         setDataReady({ loading: false, error: errorMessage })
       } else {
         await makePseudoBars(symbols) // use the extracted barebones symbols for IEX queries
-        // debugger //bcm
         // add pseudo end-of-day bar to each symbol's price series
         symbolKeys.forEach(async (symbolKey) => {
           await appendPseudoBar(symbolKey)
         })
-        // debugger //bcm
         setDataReady({ loading: false, error: errorMessage }) //finished
       }
     }
   }
-  // // Start the buildPseudoBars running in the background
-  // ;(async function() {
-  //   try {
-  //     await buildPseudoBars()
-  //   } catch (err) {
-  //     debugger // pause for developer
-  //   }
-  //   // if (errorMessage) {
-  //   //   alert(errorMessage)
-  //   // }
-  //   // debugger //bcm
-  // })()
 
   const { loading, error } = dataReady
   const divStyle = { marginTop: 80, marginLeft: 50 }
-
   if (loading) {
     ;(async function() {
       try {
