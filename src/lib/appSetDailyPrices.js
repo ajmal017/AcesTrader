83e352ca88d8c9b1c6feb6abe458d7cd7016ee8d
martinapps <bruce.martinduo@gmail.com>
2019-11-14 09:08:12 -0500

    Remove debugger statement in appSetDailyPrices.js

--- src/lib/appSetDailyPrices.js
+++ src/lib/appSetDailyPrices.js
@@ -4,6 +4,5 @@ import { getPortfolioSymbols } from './appGetPortfolioSymbols'
 export const setDailyPrices = async (state) => {
   const symbols = getPortfolioSymbols(state) // collect all the symbols in currently loaded portfolio
   const item = symbols[0]
-  debugger
   return null
 }
